626ae7bbe2854b5064fcb960bb22a03c89fb6ecd
周行 <793745175@qq.com>
2023-09-03 16:22:52 +0800

    electron-vue

---
+++ electron-vue/src/renderer/components/LandingPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <a-button type="primary">Primary Button</a-button>
+  </div>
+</template>
+
+<script>
+import SystemInformation from './LandingPage/SystemInformation'
+// import { Button } from 'ant-design-vue'
+
+export default {
+  name: 'landing-page',
+  components: { SystemInformation },
+  methods: {
+    open (link) {
+      this.$electron.shell.openExternal(link)
+    }
+  }
+}
+</script>
+
+<style>
+  #logo {
+    height: auto;
+    margin-bottom: 20px;
+    width: 420px;
+  }
+</style>
